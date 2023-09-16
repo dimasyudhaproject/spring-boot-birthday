9bd0a2716e76f24f321245bd2d4b2846daa6e123
Dimas Yudha Pratama <official@dimasyudha.tech>
2023-09-16 20:30:27 +0700

    feat: Init Happy Birthday Message project
    Signed-off-by: Dimas Yudha Pratama <“official@dimasyudha.tech”>

---
+++ src/main/java/tech/dimasyudha/birthday/HappyBirthdayMessageApplication.java
@@ -0,0 +1,13 @@
+package tech.dimasyudha.birthday;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HappyBirthdayMessageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HappyBirthdayMessageApplication.class, args);
+	}
+
+}
---
+++ src/test/java/tech/dimasyudha/birthday/HappyBirthdayMessageApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.dimasyudha.birthday;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HappyBirthdayMessageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
